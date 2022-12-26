1262b6cdf360481838cd82bd81ebeaaceb61a524
crenaz <crenaz@gmail.com>
2022-12-26 16:33:27 -0400

    433

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+
+app.post('/api', (request, response) => {
+    console.log(request);
+});
